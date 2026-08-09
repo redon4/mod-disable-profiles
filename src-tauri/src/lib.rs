332cc3a328ad2ef7890ed6a98a67b243e2675de0
redon4 <211563626+redon4@users.noreply.github.com>
2026-08-09 13:53:58 +0200

    chore: add local files

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,24 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {name}! You've been greeted from Rust!")
+}
+
+#[tauri::command]
+fn get_folder_list(path: &str) -> Vec<String> {
+    let entries = std::fs::read_dir(path).unwrap();
+    let name_vec: Vec<String> = entries
+        .filter_map(|entry| entry.ok()?.file_name().into_string().ok())
+        .collect();
+
+    name_vec
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet, get_folder_list])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/main.js
@@ -0,0 +1,23 @@
+const { invoke } = window.__TAURI__.core;
+
+let greetInputEl;
+let greetMsgEl;
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+async function get_folder_list() {
+  let list = "";
+  const nameList = await invoke("get_folder_list", { path: "/home/redon" })  
+  nameList.forEach(element => {
+    list += "<li>" + element + "</li>"
+  });
+
+  document.querySelector("#list").innerHTML = list;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".sel_folder").addEventListener("click", get_folder_list)
+});
