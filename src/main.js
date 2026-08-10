74447db123ae197de1409506d068e3821499a144
redon4 <211563626+redon4@users.noreply.github.com>
2026-08-10 18:20:08 +0200

    refactor: change indent on main.js

--- src/main.js
+++ src/main.js
@@ -4,20 +4,20 @@ let greetInputEl;
 let greetMsgEl;
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
 }
 
 async function get_folder_list() {
-  let list = "";
-  const nameList = await invoke("get_folder_list", { path: "/home/redon" })  
-  nameList.forEach(element => {
-    list += "<li>" + element + "</li>"
-  });
+    let list = "";
+    const nameList = await invoke("get_folder_list", { path: "/home/redon" })
+    nameList.forEach(element => {
+        list += "<li>" + element + "</li>"
+    });
 
-  document.querySelector("#list").innerHTML = list;
+    document.querySelector("#list").innerHTML = list;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".sel_folder").addEventListener("click", get_folder_list)
+    document.querySelector(".sel_folder").addEventListener("click", get_folder_list)
 });
